7075e2d65d48c9616d14d3fcdbefd8eeb2a2614f
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-09-11 21:32:46 +0200

    Fix npm test

--- test/test-dockercompose.js
+++ test/test-dockercompose.js
@@ -31,7 +31,7 @@ describe('JHipster generator Docker', function () {
             assert.file([
                 'Dockerfile'
             ]);
-            assert.fileContent('Dockerfile','FROM java:8');
+            assert.fileContent('Dockerfile','FROM java:openjdk-8-jdk-alpine');
         });
     });
 
